51cbb0f1b39db5334741f80a99a9f0202309b78f
21A91A6154 <97424726+21A91A6154@users.noreply.github.com>
2022-09-24 14:48:14 +0530

    import java.util.Scanner; class Array {        public static void main(String args[])       {            Scanner sc=new Scanner (System.in);            int n,i,c=0,s=0,z=0,j,k;            float avg=0;            n=sc.nextInt();           int x[]=new int[n];           for(i=0;i<n;i++)           {                x[i]=sc.nextInt();           }           k=sc.nextInt();           for(i=n-1;i>=0;i--)           {               c=0;               for(j=1;j<=x[i];j++)               {                   if(x[i]%j==0)                   {                       c+=1;                   }               }               if(c==2)               {                   if(x[i]<=k)                   {                      z+=1;                   }               }           }           System.out.print(z);            sc.close();       } }

---
+++ primes_less_than_k.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+class Array
+{
+       public static void main(String args[])
+      {
+           Scanner sc=new Scanner (System.in);
+           int n,i,c=0,s=0,z=0,j,k;
+           float avg=0;
+           n=sc.nextInt();
+          int x[]=new int[n];
+          for(i=0;i<n;i++)
+          {
+               x[i]=sc.nextInt();
+          }
+          k=sc.nextInt();
+          for(i=n-1;i>=0;i--)
+          {
+              c=0;
+              for(j=1;j<=x[i];j++)
+              {
+                  if(x[i]%j==0)
+                  {
+                      c+=1;
+                  }
+              }
+              if(c==2)
+              {
+                  if(x[i]<=k)
+                  {
+                     z+=1;
+                  }
+              }
+          }
+          System.out.print(z);
+           sc.close();
+      }
+}
