aa7db6d56c9eab147a475fde2847dcc46443457b
21A91A6154 <97424726+21A91A6154@users.noreply.github.com>
2022-09-28 00:52:05 +0530

    import java.util.Scanner; class divisible {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,k,i,a,c=0;         n=sc.nextInt();         k=sc.nextInt();         for(i=0;i<n;i++)         {             a=sc.nextInt();             if(a%k!=0)             {                 c+=1;             }         }         System.out.print(c);         sc.close();     } }

---
+++ Count_elements_not_divisible_by_K.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+class divisible
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n,k,i,a,c=0;
+        n=sc.nextInt();
+        k=sc.nextInt();
+        for(i=0;i<n;i++)
+        {
+            a=sc.nextInt();
+            if(a%k!=0)
+            {
+                c+=1;
+            }
+        }
+        System.out.print(c);
+        sc.close();
+    }
+}
