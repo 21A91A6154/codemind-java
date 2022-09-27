5ed2f5f590f696408eec8b11e7d208984bf5bc05
21A91A6154 <97424726+21A91A6154@users.noreply.github.com>
2022-09-28 00:44:14 +0530

    import java.util.Scanner; class palindrome {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in); int n,r,sum=0,temp; n=sc.nextInt(); temp=n; while(n>0) { r=n%10; sum=(sum*10)+r; n=n/10; } if(temp==sum) { System.out.print("True"); } else { System.out.printf("False"); } } }

---
+++ Palindrome.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+class palindrome   
+{    
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+int n,r,sum=0,temp;    
+n=sc.nextInt();   
+temp=n;    
+while(n>0)    
+{    
+r=n%10;    
+sum=(sum*10)+r;    
+n=n/10;    
+}    
+if(temp==sum) 
+{
+System.out.print("True"); 
+}
+else 
+{
+System.out.printf("False"); 
+}
+}
+}   
