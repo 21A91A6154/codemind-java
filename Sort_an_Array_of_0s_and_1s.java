3a484f57f6bfd0d3f5ed2e51f7c14511e0d3b4c4
21A91A6154 <97424726+21A91A6154@users.noreply.github.com>
2022-09-28 00:50:56 +0530

    import java.util.Scanner; class sort {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,i;         n=sc.nextInt();         int a[]=new int[n];         for(i=0;i<n;i++)         {             a[i]=sc.nextInt();         }         for(i=0;i<n;i++)         {             if(a[i]==0)             {                 System.out.print(a[i]+" ");             }         }         for(i=0;i<n;i++)         {             if(a[i]==1)             {                 System.out.print(a[i]+" ");             }         }     } }

---
+++ Sort_an_Array_of_0s_and_1s.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+class sort
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n,i;
+        n=sc.nextInt();
+        int a[]=new int[n];
+        for(i=0;i<n;i++)
+        {
+            a[i]=sc.nextInt();
+        }
+        for(i=0;i<n;i++)
+        {
+            if(a[i]==0)
+            {
+                System.out.print(a[i]+" ");
+            }
+        }
+        for(i=0;i<n;i++)
+        {
+            if(a[i]==1)
+            {
+                System.out.print(a[i]+" ");
+            }
+        }
+    }
+}
