fd7f9c569c8e4bdb1759d9665acf0507f02498ce
21A91A6154 <97424726+21A91A6154@users.noreply.github.com>
2022-09-28 22:39:12 +0530

    import java.util.Scanner; class move {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);     int n,i;     n=sc.nextInt();     int arr[]=new int[n];     for(i=0;i<n;i++)     {         arr[i]=sc.nextInt();     }     for(i=0;i<n;i++)     {         if(arr[i]!=0)         {             System.out.print(arr[i]+" ");         }     }     for(i=0;i<n;i++)     {         if(arr[i]==0)         {             System.out.print(arr[i]+" ");         }     }     } }

---
+++ Move_Zeroes.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+class move
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+    int n,i;
+    n=sc.nextInt();
+    int arr[]=new int[n];
+    for(i=0;i<n;i++)
+    {
+        arr[i]=sc.nextInt();
+    }
+    for(i=0;i<n;i++)
+    {
+        if(arr[i]!=0)
+        {
+            System.out.print(arr[i]+" ");
+        }
+    }
+    for(i=0;i<n;i++)
+    {
+        if(arr[i]==0)
+        {
+            System.out.print(arr[i]+" ");
+        }
+    }
+    }
+}
