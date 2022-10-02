b3a97e9a28a2d68d0bbcc4ac89a13fee68ec2f63
21A91A6154 <97424726+21A91A6154@users.noreply.github.com>
2022-10-02 21:46:26 +0530

    import java.util.Scanner; class pettern {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int i,j,n;         n=sc.nextInt();         for(i=1;i<=n;i++)         {             for(j=1;j<=n;j++)             {                 if(j==1||j==i||i==n)                 {                     System.out.print("*");                 }                 else                 {                     System.out.print(" ");                 }             }             System.out.println();         }     } }

--- print_the_following_pattern.java
+++ print_the_following_pattern.java
@@ -1,21 +1,25 @@
 import java.util.Scanner;
-class pattern3
+class pettern
 {
-   public static void main(String args[])
-   {
-         Scanner sc=new Scanner(System.in);
-         int n,i,j,k=0;
-         n=sc.nextInt();
-         k=n;
-         for(i=1;i<=n;i++)
-         {
-              for(j=1;j<=n;j++)
-              {
-                     System.out.print(k+" ");
-                     k-=1;
-              }
-              k=n;
-              System.out.println();
-         }
-   }
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int i,j,n;
+        n=sc.nextInt();
+        for(i=1;i<=n;i++)
+        {
+            for(j=1;j<=n;j++)
+            {
+                if(j==1||j==i||i==n)
+                {
+                    System.out.print("*");
+                }
+                else
+                {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
 }
